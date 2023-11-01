1f55930a0b5dde5e52a76eacc2a499d419211d4f
lucasgazzola <lucasgazzola1@outlook.com>
2023-11-01 19:22:47 -0300

    fixed styles

---
+++ src/utils/info.ts
@@ -0,0 +1,95 @@
+export const socialNetworks = [
+  {
+    network: 'GitHub',
+    info: 'github.com/lucasgazzola',
+    href: 'https://github.com/lucasgazzola',
+  },
+  {
+    network: 'LinkedIn',
+    info: 'linkedin.com/in/lucasgazzola',
+    href: 'https://www.linkedin.com/in/lucasgazzola/',
+  },
+  {
+    network: 'Email',
+    info: 'lucasgazzola@outlook.com',
+    href: 'mailto:lucasgazzola1@outlook.com',
+  },
+  {
+    network: 'WhatsApp',
+    info: '+54 364 744-6259',
+    href: 'https://wa.me/543647446259',
+  },
+]
+
+export const techStack = [
+  { id: 1, title: 'HTML' },
+  { id: 2, title: 'CSS' },
+  { id: 3, title: 'JavaScript' },
+  { id: 4, title: 'TypeScript' },
+  { id: 5, title: 'Nodejs' },
+  { id: 6, title: 'Express' },
+  { id: 7, title: 'React' },
+  { id: 8, title: 'Nextjs' },
+  { id: 9, title: 'PostgreSQL' },
+  { id: 10, title: 'MongoDB' },
+  { id: 11, title: 'Tailwind' },
+  { id: 12, title: 'GitHub' },
+  { id: 13, title: 'Prisma' },
+  { id: 14, title: 'VSCode' },
+  { id: 15, title: 'Linux' },
+]
+
+export const projects = [
+  {
+    id: 0,
+    title: 'Title of the project',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur aasdasddipisicing elit. Laborum, quaerat atque et sunt quas molestiae doloremque commodi nisi quod mollitia id recusandae facilis illum repellendus distinctio, ad, minus laboriosam amet.',
+    techs: ['React', 'Next'],
+    img: {
+      src: '',
+      alt: 'image description',
+    },
+    url: '',
+    github: '',
+  },
+  {
+    id: 1,
+    title: 'Title of the project',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, quaerat atque et sunt quas molestiae doloremque asdcommodi nisi asdasdquod mollitia id recusandae facilis illum repellendus distinctio, ad, minus laboriosam amet.',
+    techs: ['Angular', 'Node'],
+    img: {
+      src: '',
+      alt: 'image description',
+    },
+    url: '',
+    github: '',
+  },
+  {
+    id: 2,
+    title: 'Title of the project',
+    description:
+      'Lorem ipsum dolecusandae facilis illum repellendus distinctio, ad, minusasdasd laboriosam amet.',
+    techs: ['MongoDB', 'React'],
+    img: {
+      src: '',
+      alt: 'image description',
+    },
+    url: '',
+    github: '',
+  },
+  {
+    id: 3,
+    title: 'Title of the project',
+    description:
+      'Lorem ipsum dd mollitia id recusandae facilis illum repellendus distinctio, ad, minus laboriosam amet.',
+    techs: ['PostgreSQL', 'React', 'Socket.io'],
+    img: {
+      src: '',
+      alt: 'image description',
+    },
+    url: '',
+    github: '',
+  },
+]
